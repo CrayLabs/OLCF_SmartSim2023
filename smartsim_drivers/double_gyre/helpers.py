bf47ade549cd627e0a07fa00d613799880b38794
Andrew Shao <aeshao@gmail.com>
2023-07-19 19:24:43 -0500

    Remove pandas from helpers as well

--- smartsim_drivers/double_gyre/helpers.py
+++ smartsim_drivers/double_gyre/helpers.py
@@ -1,27 +1,5 @@
-import pandas as pd
 import re
 
-def parse_mom6_out(fname='MOM6_run_exp/MOM6_run/MOM6_run.out'):
-    """Retrieve timings from the MOM6 standard output"""
-    with open(fname) as f:
-        lines = f.readlines()
-
-    read = False
-    fields = ['name', 'tmin', 'tmax', 'tavg', 'tstd', 'tfrac', 'grain', 'pemin', 'pemax']
-    timings = []
-
-    for line in lines:
-        if 'Total runtime' in line:
-            read = True
-        if 'MPP_STACK' in line:
-            read = False
-        if read:
-            parsed_string = line.replace('\n','')
-            parsed_string = re.split('  +', parsed_string)
-            timings.append(parsed_string)
-
-    df = pd.DataFrame(timings,columns=fields)
-    return df
 
 def allowed_cpus(n_cpus, only_physical=True, reversed=False):
     """Return a list of the first n_cpus CPUs that are available on Frontier"""
