dda16a13cc6dbed26a1d661036b13447c3ec30d6
Andrew Shao <aeshao@gmail.com>
2023-07-19 19:19:58 -0500

    Update instructions for horizon

--- README.md
+++ README.md
@@ -6,36 +6,37 @@ numerical solver (the momentum component of the Navier-Stokes equations).
 
 # Quick Instructions
 
-1. Navigate to the workshop scratch space on the Lustre filesystem
+1.  Go to user directory on scratch
 
     ```
-    export SCRATCH_ROOT=/lustre/orion/gen150/world-shared/smartsim-2023/workshop_scratch/$USER
+    export SCRATCH_ROOT=/lus/scratch/$USER/demo_scratch
     mkdir -p $SCRATCH_ROOT
     cd $SCRATCH_ROOT
     ```
 
-1. Clone down the repository
+1. Clone down the repository and checkout horizon branch
 
     ```
     git clone https://github.com/CrayLabs/OLCF_SmartSim2023.git
+    git checkout horizon
     ```
 
 1. Get a Slurm allocation
 
     ```
-    salloc -N 1 -t 01:00:00 --reservation=smartsim -A YOUR_ALLOCATION_NAME_HERE
+    salloc -N 1 -t 01:00:00 -C P100
     ```
 
 1. Add the modulefile for SmartSim
 
     ```
-    module use $SCRATCH_ROOT/OLCF_SmartSim2023/modulefiles
+    module use /lus/scratch/smartsim/local/modulefiles
     ```
 
 1. Load the modulefile
 
     ```
-    module load py-smartsim-0.5.0-gcc-11.2.0
+    module load py-smartsim/0.5.0
     ```
 
 1. Run the MOM6 example
--- smartsim_drivers/double_gyre/call_MOM6.py
+++ smartsim_drivers/double_gyre/call_MOM6.py
@@ -11,6 +11,7 @@ from smartsim import Experiment
 
 from helpers import parse_mom6_out, allowed_cpus
 
+_DISPLAY_NAMES = ['Total runtime','(CNN total inference)']
 NUM_NODES = 1
 ML_INPUTS = "./ml_inputs"
 
@@ -19,11 +20,11 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-def main(enable_pinning):
+def main():
     timing_dfs = []
 
-    db_cpus = [24]
-    mom6_cpus = [30]
+    db_cpus = [8]
+    mom6_cpus = [8]
     refines = [3]  # Refinement relative to 1/4-degree, e.g. '3' refers to a 1/12-degree model
 
     combinations = itertools.product(db_cpus, mom6_cpus, refines)
@@ -37,16 +38,12 @@ def main(enable_pinning):
         # create and start an instance of the Orchestrator database
         # create and start an MOM6 experiment
         srun = exp.create_run_settings(
-                exe='/lustre/orion/gen150/world-shared/smartsim-2023/workshop_materials/MOM6-examples/build/ocean_only/MOM6',
+                exe='/lus/scratch/smartsim/demos/MOM6_frontier/MOM6-examples/build/ocean_only/MOM6',
                 run_command='srun'
                 )
         srun.set_nodes(NUM_NODES)
         srun.set_tasks(mom6_cpu*NUM_NODES)
         srun.set_tasks_per_node(mom6_cpu)
-
-        if enable_pinning:
-            mom6_pinning = ','.join(f'{i}' for i in allowed_cpus(mom6_cpu, reversed=True))
-            srun.set('cpu-bind',f'map_cpu:{mom6_pinning}')
 
         # start MOM6
         model = exp.create_model("MOM6_run", srun)
@@ -57,11 +54,8 @@ def main(enable_pinning):
             'use_hor_visc_python':'True'
             }
 
-        db_pinning = allowed_cpus(db_cpu) if enable_pinning else []
         model.colocate_db_uds(
-                custom_pinning=db_pinning,
                 db_cpus=db_cpu,
-                debug=True,
                 unix_socket='/tmp/redis.socket'
                 )
         model.add_ml_model(
@@ -86,25 +80,19 @@ def main(enable_pinning):
         exp.start(model, summary=False, block=True)
 
         end_time = time.time()
-        logging.info(f'Finished: db_cpu={db_cpu}\tmom6_cpu={mom6_cpu}\t{end_time-start_time}')
 
         tmp_df = parse_mom6_out(f'{exp_name}/MOM6_run/MOM6_run.out')
         tmp_df['db_cpu'] = db_cpu
         tmp_df['mom6_cpu'] = mom6_cpu
         tmp_df['refine'] = refine
+
+        print(tmp_df.loc[tmp_df['name'].isin(_DISPLAY_NAMES)])
+        logging.info(f'Finished: db_cpu={db_cpu}\tmom6_cpu={mom6_cpu}\t{end_time-start_time}')
+
         timing_dfs.append(tmp_df)
 
     full_df = pd.concat(timing_dfs, ignore_index=True)
     full_df.to_csv('timings.csv')
 
 if __name__ == "__main__":
-    parser=argparse.ArgumentParser(
-        description='Run the MOM6 double gyre test case with a CNN for turbulence'
-    )
-    parser.add_argument(
-        "--enable-pinning",
-        help="Enable pinning for both the application and the database",
-        action="store_true"
-    )
-    args = parser.parse_args()
-    main(args.enable_pinning)
+    main()
